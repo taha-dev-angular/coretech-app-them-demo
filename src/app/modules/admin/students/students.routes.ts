8a3d997820d4540117c153a0c787817f4fe1d004
taha-dev-angular <tahadevangular@gmail.com>
2024-03-28 15:56:16 +0300

    add Navigation permissions services and init

---
+++ src/app/modules/admin/students/students.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { StudentsComponent } from './students.component';
+
+export default [
+    {
+        path     : '',
+        component: StudentsComponent,
+    },
+] as Routes;
